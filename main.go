ced2725ee0095edcb31a595e22f7322a50064a53
vodish <vodish@yandex.ru>
2026-05-07 00:34:05 +0300

    up

--- main.go
+++ main.go
@@ -53,13 +53,14 @@ func main() {
 	fs := http.FileServer(http.Dir("./static"))
 	http.Handle("/", fs)
 
-	// WebSocket endpoint
+	// WebSocket ручка
 	http.HandleFunc("/ws", handleWebSocket)
 
 	log.Printf("Сервер запущен на http://localhost:%s", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
 
+// Обработчик вебсокета
 func handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
